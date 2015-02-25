b712e354dd55e2ffbb9681c3bc5abc4a0598cb21
webianks <webianks@gmail.com>
2015-02-25 16:14:05 +0530

    Project added

---
+++ app/src/main/java/com/webi/material/About.java
@@ -0,0 +1,23 @@
+package com.webi.material;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+
+@SuppressLint("NewApi")
+public class About extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+		
+		
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().hide();
+      }
+		
+	}
+
+}
