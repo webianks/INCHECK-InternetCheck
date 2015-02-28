cef8c9d4b4f0c8f42ff63dd5ab6bcb5f6b8a8e6f
manishSRM <manishchaurasiya101@gmail.com>
2015-02-28 15:11:54 +0530

    final

--- app/src/main/java/com/webi/material/MainActivity.java
+++ app/src/main/java/com/webi/material/MainActivity.java
@@ -48,7 +48,7 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity implements OnClickListener {
-	Button bt;
+	Button bt, Button1;
     ProgressBar pb;
     URLExistAsyncTask task;
     RelativeLayout rl,cl,rlBottom;
